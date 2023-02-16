80e65eb9efc635b8930ad7425127e954b127022a
David Freddy <111796950+frd2377@users.noreply.github.com>
2023-02-16 01:04:47 -0500

    Update db.js

--- models/db.js
+++ models/db.js
@@ -7,7 +7,7 @@ const Imagenes = db.define('Imagenes',{
         type: DataTypes.STRING
     },
     url:{
-        type: DataTypes.STRING(700)
+        type: DataTypes.STRING({length:700})
     }
 })
 
